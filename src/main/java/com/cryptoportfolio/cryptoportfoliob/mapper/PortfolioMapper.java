57130ddc46d272b703486edbf186001c6a26a309
Parthiban D <44821026+parthibandayalan@users.noreply.github.com>
2021-01-15 13:04:00 +0530

    reset done to last night version and updated pom.xml

--- src/main/java/com/cryptoportfolio/cryptoportfoliob/mapper/PortfolioMapper.java
+++ src/main/java/com/cryptoportfolio/cryptoportfoliob/mapper/PortfolioMapper.java
@@ -10,5 +10,8 @@ import com.cryptoportfolio.cryptoportfoliob.entity.Portfolio;
 @Mapper(componentModel = "spring")
 public interface PortfolioMapper {	
 
+	@Mapping(target = "coins", ignore = true)
+	Portfolio toEntity(UserRegistrationDTO registrationDTO);
+	
 	PortfolioDTO toDTO(Portfolio portfolio);
 }
