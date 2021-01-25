94bf4e5bb5e9b513b4595d0ff2d2e3fab74d20b1
Parthiban D <44821026+parthibandayalan@users.noreply.github.com>
2021-01-26 00:25:37 +0530

    https set to true

--- src/main/java/com/cryptoportfolio/cryptoportfoliob/controllers/AuthenticationController.java
+++ src/main/java/com/cryptoportfolio/cryptoportfoliob/controllers/AuthenticationController.java
@@ -49,7 +49,7 @@ public class AuthenticationController {
 		
 		Cookie cookie = new Cookie("jwt", jwt);
 		cookie.setMaxAge(30*60);
-		cookie.setSecure(false);//----------> need to set this as false for localhost connections. set as true for https connections 
+		cookie.setSecure(true);//----------> need to set this as false for localhost connections. set as true for https connections 
 		cookie.setPath("/");
 		cookie.setHttpOnly(true);		
 
