686607054811fd73ba7d314fe3ce3fd11b67b4e4
Parthiban D <44821026+parthibandayalan@users.noreply.github.com>
2021-01-28 12:23:03 +0530

    added exceptions for options

--- src/main/java/com/cryptoportfolio/cryptoportfoliob/security/CorsConfig.java
+++ src/main/java/com/cryptoportfolio/cryptoportfoliob/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:3000","https://192.168.1.11:3000")
+					.allowedOrigins("http://localhost:3000","https://192.168.1.11:3000","https://pdcryptoportfolio.herokuapp.com/")
 					.allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAD")
 					.allowedHeaders("*")
 					.allowCredentials(true).exposedHeaders("*");
--- src/main/java/com/cryptoportfolio/cryptoportfoliob/security/SecurityConfigurer.java
+++ src/main/java/com/cryptoportfolio/cryptoportfoliob/security/SecurityConfigurer.java
@@ -1,11 +1,8 @@
 package com.cryptoportfolio.cryptoportfoliob.security;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,9 +12,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 
@@ -49,6 +43,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
+		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 		.antMatchers("/hello").permitAll()
 		.anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
