f9efca417ec1ca5f6a05e2ba1c293fda24d68e33
Parthiban D <44821026+parthibandayalan@users.noreply.github.com>
2021-01-28 16:28:08 +0530

    Spring Security Settings Changed

--- src/main/java/com/cryptoportfolio/cryptoportfoliob/security/CorsConfig.java
+++ src/main/java/com/cryptoportfolio/cryptoportfoliob/security/CorsConfig.java
@@ -6,20 +6,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-	@Bean
-	public WebMvcConfigurer getCorsConfiguration() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:3000","https://192.168.1.11:3000","https://pdcryptoportfolio.herokuapp.com/")
-					.allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAD")
-					.allowedHeaders("*")
-					.allowCredentials(true).exposedHeaders("*");
-			}
-		};
-	}
-	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH","OPTIONS");
+    }
 }
--- src/main/java/com/cryptoportfolio/cryptoportfoliob/security/SecurityConfigurer.java
+++ src/main/java/com/cryptoportfolio/cryptoportfoliob/security/SecurityConfigurer.java
@@ -1,5 +1,7 @@
 package com.cryptoportfolio.cryptoportfoliob.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 
@@ -42,8 +47,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
-		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()		
 		.antMatchers("/hello").permitAll()
 		.anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
@@ -52,6 +56,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 //	      .requiresSecure();
 		
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		http.cors();
 		
 		//http.headers()..frameOptions().sameOrigin();
 	}
@@ -70,5 +75,22 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 //	    source.registerCorsConfiguration("/**", configuration);
 //	    return source;
 //	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://192.168.1.11:3000","https://pdcryptoportfolio.herokuapp.com/"));     
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
